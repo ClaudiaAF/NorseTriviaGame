07ecb8c01c08ffd57b7f98caeed8af5146329494
claud <180181@virtualwindow.co.za>
2021-03-03 22:19:57 +0200

    Categories page created, display name functional

--- app/src/main/java/com/example/norsetriviagame/CategoriesPage.kt
+++ app/src/main/java/com/example/norsetriviagame/CategoriesPage.kt
@@ -4,17 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
 import android.widget.TextView
 
 class CategoriesPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories_page2)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
 
         val categorieshi = findViewById<TextView>(R.id.categories_hi)
 
         val userName = intent.getStringExtra(EXTRA_MESSAGE)
 
-        categorieshi.text = "Hello " + userName
+        categorieshi.text = "Greetings " + userName
     }
 }
