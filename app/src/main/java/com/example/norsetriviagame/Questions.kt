7e186dca2a1b9b2552d5a68e5e58485c9aeba4f7
claud <180181@virtualwindow.co.za>
2021-03-08 00:22:27 +0200

    Questions for Gods category created

---
+++ app/src/main/java/com/example/norsetriviagame/Questions.kt
@@ -0,0 +1,10 @@
+package com.example.norsetriviagame
+
+data class Questions (
+        val id: Int,
+        val question: String,
+        val icon: Int,
+        val optionOne: String,
+        val optionTwo: String,
+        val optionThree: String
+)
