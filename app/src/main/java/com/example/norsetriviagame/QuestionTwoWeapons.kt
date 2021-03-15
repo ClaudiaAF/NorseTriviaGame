1bb95042bb45c9556b8ec306798ffc8122f28842
claud <180181@virtualwindow.co.za>
2021-03-16 01:56:53 +0200

    Question 2 in Weapons styled

--- app/src/main/java/com/example/norsetriviagame/QuestionTwoWeapons.kt
+++ app/src/main/java/com/example/norsetriviagame/QuestionTwoWeapons.kt
@@ -3,45 +3,45 @@ package com.example.norsetriviagame
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.RadioButton
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_question_one_places.*
-import kotlinx.android.synthetic.main.activity_question_one_weapons.*
+import kotlinx.android.synthetic.main.activity_question_two_gods.*
+import kotlinx.android.synthetic.main.activity_question_two_weapons.*
 
-class QuestionOneWeapons : AppCompatActivity() {
+class QuestionTwoWeapons : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_question_one_weapons)
+        setContentView(R.layout.activity_question_two_weapons)
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
-        val userName = intent.getStringArrayExtra(Constants.USER_NAME)
+        val userName = intent.getStringExtra(Constants.USER_NAME)
 
         val questionsList = Constants.getWeaponryQuestions()
-        Log.i("QuestionsList: ", "${questionsList.size}")
 
         //set question number and array
-        val questionNumber: Int = 1
+        val questionNumber: Int = 2
         val question: Questions = questionsList[questionNumber-1]
 
         //set UI elements to question 1
-        tv_question_weapons.text = question.question
-        rb_answer_one_weapons.text = question.optionOne
-        rb_answer_two_weapons.text = question.optionTwo
-        rb_answer_three_weapons.text = question.optionThree
+        tv_question_weapons2.text = question.question
+        rb_answer_one_weapons2.text = question.optionOne
+        rb_answer_two_weapons2.text = question.optionTwo
+        rb_answer_three_weapons2.text = question.optionThree
 
-        pb_progressBar_weapons.progress = questionNumber
-        tv_progress_weapons.text = questionNumber.toString() + "/" + questionsList.size.toString()
+        pb_progressBar_weapons2.progress = questionNumber
+        tv_progress_weapons2.text = questionNumber.toString() + "/" + questionsList.size.toString()
 
         //set a button on click listener
         var answers: RadioButton
         var correctAnswer: Int = 0
 
-        btn_next_weapons.setOnClickListener{
-            var id: Int = rg_options_weapons.checkedRadioButtonId
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWER, 0)
+        tv_correctAnswer_weapons2.text = correctAnswers.toString()
 
+        btn_next_weapons2.setOnClickListener{
+            var id: Int = rg2_options.checkedRadioButtonId
             if(id != -1){
                 //capture answer
                 answers = findViewById(id)
@@ -54,9 +54,11 @@ class QuestionOneWeapons : AppCompatActivity() {
                 if (answers.text == question.optionTwo){
                     correctAnswer++
                 }
-//
 
-                val intent = Intent(this, QuestionTwoWeapons::class.java)
+                val correctAnswer = intent.getIntExtra(Constants.CORRECT_ANSWER, 0)
+                tv_correctAnswer_weapons2.text = correctAnswer.toString()
+
+                val intent = Intent(this, QuestionThreeGods::class.java)
                 intent.putExtra(Constants.CORRECT_ANSWER, correctAnswer)
                 startActivity(intent)
                 finish()
@@ -64,6 +66,8 @@ class QuestionOneWeapons : AppCompatActivity() {
                 // if no button checked
                 Toast.makeText(this,"Please select your answer", Toast.LENGTH_SHORT).show()
             }
+
+
         }
     }
 }
